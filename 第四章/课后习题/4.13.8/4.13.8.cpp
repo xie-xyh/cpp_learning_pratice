548ec7eaf1e047e6ecf41fdc881c20e90f0d5772
xie-xyh <1378197551@qq.com>
2024-08-30 11:00:03 +0800

    增加了第四章剩余的课后习题

--- 第四章/课后习题/4.13.8/4.13.8.cpp
+++ 第四章/课后习题/4.13.8/4.13.8.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <cstring>
+#include <string>
+
+using namespace std;
+struct pizza
+{
+    /* data */
+    string name;      //公司名字
+    double diameter;  //直径
+    double weight;    //重量
+};
+
 int main()
 {
-    using namespace std;
-    std::cout << "hello world" << std::endl;
+    pizza *p1 = new pizza;
+    cout << "enter the diameter of pizza: ";
+    cin >> p1 -> diameter;
+    cout << "enter thr weight of pizza: ";
+    cin >> p1 -> weight;
+
+    cin.get(); //读取回车
+    cout << "enter the name of pizza company: ";
+    getline(cin,p1 -> name);
+
+    cout << "the name is " << p1 -> name 
+         << ", the diameter is " <<  p1 -> diameter
+         << ", the weight is: " << p1 -> weight
+         << endl;
+
+    return 0;
 }
